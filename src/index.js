d80381a5a97bcc373cea1e29f2fafb046a8754b1
whvt <60581769+whvt@users.noreply.github.com>
2020-03-11 00:35:28 +0300

    done typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return Math.min(...array);
 }
-
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) return 0; else {
+  var sum = 0;
+for( let i = 0; i < array.length; i++ ){
+    sum += parseInt( array[i], 10 ); 
+}
+
+var avg = sum/array.length;
+return avg;
+} 
 }
